219ef5e935d9031238d9e20a860f3815f45f2e88
fabio <fabiosantosmanarin@gmail.com>
2019-10-17 23:00:13 -0300

    Aula sobre objetos

---
+++ exercicios-js/objeto/oo.js
@@ -0,0 +1,22 @@
+// CÓDIGO NÃO EXECUTÁVEL
+
+// Procedural
+processamento(valor1, valor2, valor3)
+
+// OO
+
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        // ...
+    }
+}
+
+objeto.processamento() // Foco passou a ser objeto
+
+// Pricipais conceitos
+// 1- Abstração
+// 2- Encapsulamento
+// 3- 
